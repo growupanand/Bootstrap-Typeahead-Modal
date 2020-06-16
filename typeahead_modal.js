03c6854571de2373e696450f6788272694262e96
UTKARSH ANAND <growupanand@gmail.com>
2020-06-16 21:32:05 +0530

    updated function
    data column is replaced with query column and also used for WHERE columns.
    in result now showing data with column names in table.

--- typeahead_modal.js
+++ typeahead_modal.js
@@ -1,7 +1,7 @@
   //function to call typeahead
-  function typeahead_modal(link, input_id, table_name, data_column) {
+  function typeahead_modal(link, input_id, table_name, query_column) {
     document.typeahead_form.table_name.value = table_name
-    document.typeahead_form.data_column.value = data_column
+    document.typeahead_form.query_column.value = query_column
     document.typeahead_form.input_id.value = input_id
     document.typeahead_form.link.value = link
     $('#typeahead_modal').modal('show')
@@ -10,17 +10,24 @@
 
   //function to get data list from ajax
   function typeahead_getdata() {
-             html='';
-			 query=document.typeahead_form.query.value.trim() ;
+             html='<table><tr>';
+			 query_value=document.typeahead_form.query_value.value.trim() ;
 			 link = document.typeahead_form.link.value;
 			 table_name = document.typeahead_form.table_name.value ;
 			 input_id = document.typeahead_form.input_id.value;
-			 data_column = document.typeahead_form.data_column.value
+			 query_column = document.typeahead_form.query_column.value
 	         result_table=document.getElementById('typeahead_result_list');
+	         query_array = query_column.split(",");
 
-	         if (query != '') {
+	         for (i in query_array ) {
+	         html = html+"<th>"+query_array[i]+"</th>";
+	         }
+	         html = html+"<th>ACTION</th></tr>"
+
+
+	         if (query_value != '') {
 	            $.ajax({
-			        data:{'task':'typeahead','table_name':table_name, 'data_column': data_column, 'query':query},
+			        data:{'task':'typeahead','table_name':table_name, 'query_column': query_column, 'query_value':query_value},
                     type: 'post',
                     url: link,
 			        dataType:'JSON',
@@ -30,7 +37,11 @@
                     else {
 			         for (i in result) {
 				     sn++;
-			    	 html=html+'<button onclick="typeahead_insert_data(\''+input_id+'\',\''+result[i].name+'\')" class="list-group-item list-group-item-action">'+result[i].name+'</button>';
+				     html=html+'<tr>';
+				     for (j in query_array ) {
+			    	 html=html+'<td>'+result[i][query_array[j]]+'</td>';
+			         }
+			         html=html+'<td><button onclick="typeahead_insert_data(\''+input_id+'\',\''+result[i].ID+'\')" class="btn btn-link">SELECT</button></td></tr>';
 			         }
 			        $(result_table).html(html);
 			         }
